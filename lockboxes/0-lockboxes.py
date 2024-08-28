bfbedd2280f66ff12f4d79095e0acd22d1aa91ec
Fpierr <fanuelp9@gmail.com>
2024-08-28 13:29:03 +0200

    determine if all boxes can be opened

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -9,20 +9,20 @@ def canUnlockAll(boxes):
 
     Parameters:
     boxes (list of list of int): A list where each element is a list of keys, each key is an integer.
-    
+
     Returns:
     bool: True if all boxes can be opened, False otherwise.
     """
     n = len(boxes)
     unlocked = set([0])
     queue = [0]
-    
+
     while queue: 
         current_box = queue.pop(0)
-        
+
         for key in boxes[current_box]: 
             if key < n and key not in unlocked:
                 unlocked.add(key)
                 queue.append(key)
-                
+
     return len(unlocked) == n
