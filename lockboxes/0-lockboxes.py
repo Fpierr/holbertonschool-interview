2741918434d75a78cc847157bc6104e15c05d399
Fpierr <fanuelp9@gmail.com>
2024-08-28 13:33:55 +0200

    determine if all boxes can be opened

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -3,12 +3,13 @@
 """A method that determines if all the boxes can be opened."""
 
 
-def canUnlockAll(boxes): 
+def canUnlockAll(boxes):
     """
     Determines if all boxes can be opened.
 
     Parameters:
-    boxes (list of list of int): A list where each element is a list of keys, each key is an integer.
+    boxes (list of list of int): A list where each element
+    is a list of keys, each key is an integer.
 
     Returns:
     bool: True if all boxes can be opened, False otherwise.
@@ -17,10 +18,10 @@ def canUnlockAll(boxes):
     unlocked = set([0])
     queue = [0]
 
-    while queue: 
+    while queue:
         current_box = queue.pop(0)
 
-        for key in boxes[current_box]: 
+        for key in boxes[current_box]:
             if key < n and key not in unlocked:
                 unlocked.add(key)
                 queue.append(key)
