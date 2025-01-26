401700ad29148ee0c97ac3fcf34e6b07f4b1f762
Fpierr <fanuelp9@gmail.com>
2025-01-26 19:26:56 +0100

    min operation

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Min Operation"""
+
+def minOperations(n):
+    """calculate fewest number of operation"""
+    if n <= 1:
+        return 0
+    operations = 0
+    divisor = 2
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+
+    return operations
