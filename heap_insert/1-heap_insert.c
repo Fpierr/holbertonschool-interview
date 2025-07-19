265a70a944336d60a5312ef401983795c3ddcb9c
Fpierr <fanuelp9@gmail.com>
2025-07-19 20:24:13 +0200

    add binarytree

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -1,21 +1,29 @@
+#include <stdlib.h>
 #include "binary_trees.h"
-
 /**
  * heapify_up - Restores Max Heap property by swapping with parents
  * @node: Pointer to the node to heapify
  * Return: New position of node after heapifying
  */
+
 heap_t *heapify_up(heap_t *node)
 {
+	heap_t *parent;
 	int temp;
 
-	while (node->parent && node->n > node->parent->n)
+	if (!node)
+		return (NULL);
+
+	parent = node->parent;
+	while (parent && node->n > parent->n)
 	{
-		temp = node->n;
-		node->n = node->parent->n;
-		node->parent->n = temp;
-		node = node->parent;
+		temp = parent->n;
+		parent->n = node->n;
+		node->n = temp;
+		node = parent;
+		parent = node->parent;
 	}
+
 	return (node);
 }
 
@@ -24,33 +32,30 @@ heap_t *heapify_up(heap_t *node)
  * @root: Pointer to the root node
  * Return: Pointer to parent node for insertion
  */
-heap_t *find_insert_parent(heap_t *root)
+heap_t *find_insertion_point(heap_t *root)
 {
-	heap_t **queue, *current;
-	size_t front = 0, rear = 0, size = 1024;
+	heap_t *queue[1024];
+	int head = 0, tail = 0;
+	heap_t *current;
 
 	if (!root)
 		return (NULL);
 
-	queue = malloc(sizeof(heap_t *) * size);
-	if (!queue)
-		return (NULL);
+	queue[tail++] = root;
 
-	queue[rear++] = root;
-
-	while (front < rear)
+	while (head < tail)
 	{
-		current = queue[front++];
-
+		current = queue[head++];
 		if (!current->left || !current->right)
-			break;
+			return (current);
 
-		queue[rear++] = current->left;
-		queue[rear++] = current->right;
+		if (current->left)
+			queue[tail++] = current->left;
+		if (current->right)
+			queue[tail++] = current->right;
 	}
 
-	free(queue);
-	return (current);
+	return (NULL);
 }
 
 /**
@@ -61,7 +66,7 @@ heap_t *find_insert_parent(heap_t *root)
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-	heap_t *new_node, *parent;
+	heap_t *new_node, *insert_point;
 
 	if (!root)
 		return (NULL);
@@ -72,19 +77,18 @@ heap_t *heap_insert(heap_t **root, int value)
 		return (*root);
 	}
 
-	parent = find_insert_parent(*root);
-	if (!parent)
+	insert_point = find_insertion_point(*root);
+	if (!insert_point)
 		return (NULL);
 
-	new_node = binary_tree_node(parent, value);
+	new_node = binary_tree_node(insert_point, value);
 	if (!new_node)
 		return (NULL);
 
-	if (!parent->left)
-		parent->left = new_node;
+	if (!insert_point->left)
+		insert_point->left = new_node;
 	else
-		parent->right = new_node;
+		insert_point->right = new_node;
 
 	return (heapify_up(new_node));
 }
-
