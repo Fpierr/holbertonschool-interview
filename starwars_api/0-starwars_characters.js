69232a177af88e772af0532f0090ae45a94734da
Fpierr <fanuelp9@gmail.com>
2025-07-20 02:14:48 +0200

    add star wars characters

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,30 +1,37 @@
 #!/usr/bin/node
 
-
 const https = require('https');
-
 
 const id = process.argv[2];
 if (!id) {
-	console.error('Usage: ./0-starwars_characters.js <movie_id>');
-	process.exit(1);
+  console.error('Usage: ./0-starwars_characters.js <movie_id>');
+  process.exit(1);
 }
 
-
-function fetch (url) {
+function fetch(url) {
   return new Promise(resolve => {
     https.get(url, res => {
       let data = '';
-      res.on('data', chunk => (data += chunk));
-      res.on('end', () => resolve(JSON.parse(data)));
+      res.on('data', chunk => {
+        data += chunk;
+      });
+      res.on('end', () => {
+        resolve(JSON.parse(data));
+      });
     });
   });
 }
 
-
 fetch(`https://swapi-api.hbtn.io/api/films/${id}/`)
-  .then(film => film.characters.reduce((p, url) =>
-    p.then(() => fetch(url).then(c => console.log(c.name))),
-    Promise.resolve()
-  ));
+  .then(film =>
+    film.characters.reduce(
+      (p, url) =>
+        p.then(() =>
+          fetch(url).then(character => {
+            console.log(character.name);
+          })
+        ),
+      Promise.resolve()
+    )
+  );
 
