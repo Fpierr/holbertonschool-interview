3548f62e2b8380074c3894f5f0b1bd13904dfbde
Fpierr <fanuelp9@gmail.com>
2025-07-20 02:07:14 +0200

    add star wars characters

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,22 +1,23 @@
 #!/usr/bin/node
 
-const https = require('https')
+const https = require('https');
 
-const id = process.argv[2]
-if (!id) return console.error('Usage: ./0-starwars_characters.js <movie_id>')
+const id = process.argv[2];
+if (!id) return console.error('Usage: ./0-starwars_characters.js <movie_id>');
 
-function fetch(url) {
-    return new Promise(resolve=> {
-        https.get(url, res= > {
-            let data = ''
-            res.on('data', chunk=> (data += chunk))
-            res.on('end', ()= > resolve(JSON.parse(data)))
-        })
-    })
+function fetch (url) {
+  return new Promise(resolve => {
+    https.get(url, res => {
+      let data = '';
+      res.on('data', chunk => (data += chunk));
+      res.on('end', () => resolve(JSON.parse(data)));
+    });
+  });
 }
 
-fetch(`https: // swapi-api.hbtn.io/api/films /${id} /`)
-.then(film=> film.characters.reduce((p, url) = >
-                                     p.then(() = > fetch(url).then(c = > console.log(c.name))),
-                                     Promise.resolve()
-                                     ))
+fetch(`https://swapi-api.hbtn.io/api/films/${id}/`)
+  .then(film => film.characters.reduce((p, url) =>
+    p.then(() => fetch(url).then(c => console.log(c.name))),
+    Promise.resolve()
+  ));
+
