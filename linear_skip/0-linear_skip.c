f5b6d54fcd1bde1f0cb9e271df51cb0abf4fca10
Fpierr <fanuelp9@gmail.com>
2025-07-20 03:19:56 +0200

    add serch

--- linear_skip/0-linear_skip.c
+++ linear_skip/0-linear_skip.c
@@ -9,34 +9,38 @@
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-    skiplist_t *express = list;
+	skiplist_t *express;
 
-    if (!list)
-        return (NULL);
-
-    while (express->express && express->express->n < value)
-    {
-        express = express->express;
-        printf("Value checked at index [%lu] = [%d]\n", express->index, express->n);
-    }
-
-    /* Afficher la dernière vérification express (même si >= value) */
-    if (express->express)
-    {
-        express = express->express;
-        printf("Value checked at index [%lu] = [%d]\n", express->index, express->n);
-    }
-
-    printf("Value found between indexes [%lu] and [%lu]\n", list->index, express->index);
-
-    /* Recherche linéaire classique entre list et express */
-    while (list && list->index <= express->index)
-    {
-        printf("Value checked at index [%lu] = [%d]\n", list->index, list->n);
-        if (list->n == value)
-            return (list);
-        list = list->next;
-    }
-
-    return (NULL);
+	if (list != NULL)
+	{
+		express = list->express;
+		printf("Value checked at index [%lu] = [%d]\n", express->index,
+			   express->n);
+		while (express != NULL && express->n < value)
+		{
+			list = express;
+			express = express->express;
+			if (express == NULL)
+				break;
+			printf("Value checked at index [%lu] = [%d]\n", express->index,
+				   express->n);
+		}
+		if (express == NULL)
+		{
+			express = list;
+			while (express->next != NULL)
+				express = express->next;
+		}
+		printf("Value found between indexes [%lu] and [%lu]\n", list->index,
+			   express->index);
+		while (list != NULL && list->n <= express->n)
+		{
+			printf("Value checked at index [%lu] = [%d]\n", list->index,
+				   list->n);
+			if (list->n == value)
+				return (list);
+			list = list->next;
+		}
+	}
+	return (NULL);
 }
