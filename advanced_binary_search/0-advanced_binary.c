25f2a943fc1b003047e7d4ed496bdaf4b7b366ef
Fpierr <fanuelp9@gmail.com>
2025-08-08 01:43:26 +0200

    Add advenced search

--- advanced_binary_search/0-advanced_binary.c
+++ advanced_binary_search/0-advanced_binary.c
@@ -17,6 +17,7 @@ int advanced_binary_recursive(int *array, size_t left, size_t right, int value)
 	if (right < left)
 		return (-1);
 
+	printf("Searching in array: ");
 	for (i = left; i < right; i++)
 		printf("%d, ", array[i]);
 	printf("%d\n", array[i]);
