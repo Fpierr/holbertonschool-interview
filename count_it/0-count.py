76359141d36c17ad68a3c484d97cd324d72dfc71
Fpierr <fanuelp9@gmail.com>
2025-08-13 17:53:33 +0200

    fixing error

--- count_it/0-count.py
+++ count_it/0-count.py
@@ -6,10 +6,10 @@ Usage from 0-main.py:
     count_words(subreddit, ['python', 'java', 'javascript'])
 """
 
-from __future__ import annotations
-from typing import Dict, List, Optional
 import re
 import requests
+from __future__ import annotations
+from typing import Dict, List, Optional
 
 
 def count_words(subreddit: str,
