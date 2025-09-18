4ea018e8b5a3d95bceed1326b73b65b8ed2e8f9c
Fpierr <fanuelp9@gmail.com>
2025-09-19 00:35:18 +0200

    add

--- merge_sort/0-merge_sort.c
+++ merge_sort/0-merge_sort.c
@@ -1,66 +1,61 @@
 #include "sort.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * merge - merges two subarrays into one sorted array
- * @array: the original array
- * @left: pointer to left subarray
- * @right: pointer to right subarray
- * @l_size: size of left subarray
- * @r_size: size of right subarray
- */
-void merge(int *array, int *left, int *right, size_t l_size, size_t r_size)
+ * merge - merge two sorted subarrays array
+ * @size: size of the array
+ * @array: pointer to the head of the array
+ * @left: pointer to left array
+ * @right: pointer to right array
+ **/
+
+void merge(size_t size, int *array, int *left, int *right)
 {
-	size_t i = 0, j = 0, k = 0;
-	
+	int i, j, k, l_lengh, r_lengh;
+
+	i = j = k = 0;
+	l_lengh = size / 2;
+	r_lengh = size - l_lengh;
+
 	printf("Merging...\n");
 	printf("[left]: ");
-	print_array(left, l_size);
+	print_array(left, l_lengh);
 	printf("[right]: ");
-	print_array(right, r_size);
+	print_array(right, r_lengh);
 
-	while (i < l_size && j < r_size)
-	{
-		if (left[i] <= right[j])
-			array[k++] = left[i++];
-		else
-			array[k++] = right[j++];
-	}
-	while (i < l_size)
+	while (i < l_lengh && j < r_lengh)
+		array[k++] = (left[i] < right[j]) ? left[i++] : right[j++];
+
+	while (i < l_lengh)
 		array[k++] = left[i++];
-	while (j < r_size)
+
+	while (j < r_lengh)
 		array[k++] = right[j++];
+
 	printf("[Done]: ");
-	print_array(array, l_size + r_size);
-	
+	print_array(array, size);
 }
 
 /**
- * merge_sort - sorts an array using top-down merge sort
- * @array: the array to sort
- * @size: number of elements in array
- */
+ * merge_sort - sorts an array of integers in ascending order using Merge Sort
+ * @array: pointer to the head of the array
+ * @size: size of the array
+ **/
+
 void merge_sort(int *array, size_t size)
 {
-	size_t mid, i;
-	int *left, *right;
+	size_t middle, i;
+	int left[MAX], right[MAX];
+
 	if (!array || size < 2)
 		return;
-	mid = size / 2;
-	left = malloc(mid * sizeof(int));
-	right = malloc((size - mid) * sizeof(int));
 
-	if (!left || !right)
-		return;
-	for (i = 0; i < mid; i++)
+	for (i = 0, middle = size / 2; i < middle; i++)
 		left[i] = array[i];
-	for (i = mid; i < size; i++)
-		right[i - mid] = array[i];
-	merge_sort(left, mid);
-	merge_sort(right, size - mid);
-	merge(array, left, right, mid, size - mid);
 
-	free(left);
-	free(right);
+	for (i = middle; i < size; i++)
+		right[i - middle] = array[i];
+
+	merge_sort(left, middle);
+	merge_sort(right, size - middle);
+	merge(size, array, left, right);
 }
--- merge_sort/sort.h
+++ merge_sort/sort.h
@@ -1,6 +1,8 @@
 #ifndef SORT_H
 #define SORT_H
 
+#define SIZE_MAX 2000
+
 #include <stddef.h>
 #include <stdio.h>
 
